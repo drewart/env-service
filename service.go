98755cca13bc53e8eb888ece3f371bece7e55c37
Drew Pierce <drpierce@starbucks.com>
2024-01-24 10:53:06 -0800

    adding log output

--- service.go
+++ service.go
@@ -12,15 +12,19 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if env == "" {
 		env = "unknown"
 	}
-	var headers string
+	var headers, headersFlat string
 	// Loop over header names
 	headers += "\n"
+	headersFlat += "["
 	for name, values := range r.Header {
 		// Loop over all values for the name.
 		for _, value := range values {
 			headers += "\t" + name + ":" + value + "\n"
+			headersFlat += name + ":" + value + ","
 		}
 	}
+	headersFlat += "]"
+	log.Println("Env: ", env, "Host: ", r.Host, "Path: ", r.URL.Path, "Headers: ", headersFlat)
 	fmt.Fprintf(w, "Env: %s \nHost: %s\nPath: %s\nHeaders:%s", env, r.Host, r.URL.Path, headers)
 
 }
